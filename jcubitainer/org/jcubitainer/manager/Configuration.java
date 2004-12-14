27abb629bcd1a34d7a6e74d4665d4a503969c4d4
metalm <metalm>
2004-12-14 05:45:25 +0000

    nouvelle version

--- jcubitainer/org/jcubitainer/manager/Configuration.java
+++ jcubitainer/org/jcubitainer/manager/Configuration.java
@@ -38,7 +38,7 @@ public class Configuration extends Properties {
 
 	public static final String DIR = ".jcubitainer";
 
-	public static final String VERSION = "0.2.4";
+	public static final String VERSION = "0.3";
 
 	/**
 	 *  
