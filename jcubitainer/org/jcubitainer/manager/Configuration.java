5572ba51753d333bf9058ca83f4d2c49478474a3
metalm <metalm>
2005-01-18 19:44:52 +0000

    *** empty log message ***

--- jcubitainer/org/jcubitainer/manager/Configuration.java
+++ jcubitainer/org/jcubitainer/manager/Configuration.java
@@ -38,7 +38,7 @@ public class Configuration extends Properties {
 
     public static final String DIR = ".jcubitainer";
 
-    public static final String VERSION = "0.3alpha";
+    public static final String VERSION = "0.3";
 
     public static final String NAME_VERSION = "VERSION";
 
